b9febd65e5ff435cb11fa23fdbd8aabc3ea8c44e
wadsaek <wadsaek@gmail.com>
2024-11-11 17:23:58 +0200

    Irena: 05.11

---
+++ Irena/05.11.2024/LinkedList.md
@@ -0,0 +1,12 @@
+# Linked List
+
+```
+class LinkedList<T>{
+    Node<T> start;
+}
+
+class Node<T>{
+    T value;
+    Node<T> next;
+}
+```
---
+++ Irena/05.11.2024/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _05._11._2024;
+
+class Program
+{
+    static void Main(string[] args) {
+        LinkedListNode<int> n = new LinkedListNode<int>(5);      
+        ref int val = ref n.ValueRef;
+        val = 6;
+        Console.WriteLine(n.Value);
+    }
+}
