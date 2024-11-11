9aa9c648486bbef940a0b46bd6f7a387bc427ccb
wadsaek <wadsaek@gmail.com>
2024-11-11 17:23:09 +0200

    OOP: 03.11

---
+++ OOP/03.11.2024/Point.cs
@@ -0,0 +1,45 @@
+﻿namespace _03._11._2024;
+
+public class Point
+{
+    int x;
+    int y;
+
+    public Point(){}
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int GetX(){
+        return x;
+    }
+    public Point SetX(int x){
+        this.x = x;
+        return this;
+    }
+
+    public int GetY(){
+        return y;
+    }
+    public Point SetY(int y){
+        this.y = y;
+        return this;
+    }
+
+    public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+}
+
+public static class PointArrExtension{
+    public static string ArrToStr(this Point[] arr){
+        string a = "[";
+        foreach(Point i in arr){
+            a+=$"{i} ";
+        }
+        a+="]";
+        return a;
+    }
+}
---
+++ OOP/03.11.2024/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._11._2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point[] arr = new Point[4];
+
+        Point p = new Point(1,2);
+        arr[0] = new Point(5,6);
+        arr[1] = p;
+        arr[2] = new Point(
+                arr[0].GetX(),
+                arr[0].GetY()
+                );
+        Console.WriteLine(arr.ArrToStr());
+    }
+}
