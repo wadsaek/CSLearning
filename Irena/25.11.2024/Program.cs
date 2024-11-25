3121f59f3f0b78b02ca545e70f2bab09972f6719
wadsaek <wadsaek@gmail.com>
2024-11-25 13:27:09 +0200

    Library: init

---
+++ Irena/25.11.2024/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Unit4.CollectionsLib;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 54, 2, 6 };
+            Node<int> node = arr.BuildList();
+            Console.WriteLine(node.ToStringRecursive());
+            node.Delete(node.GetNext().GetNext());
+            Console.WriteLine("delete third element");
+            Console.WriteLine(node.ToStringRecursive());
+        }
+    }
+}
