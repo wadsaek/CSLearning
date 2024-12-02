83de3990892c555da0d2aaa8b41713638d8db50f
wadsaek <wadsaek@gmail.com>
2024-12-02 16:27:19 +0200

    Library: update

---
+++ Irena/Library/ListExtension.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using Unit4.CollectionsLib;
+
+namespace Library;
+
+public static class ListExtension {
+    public static Node<T> BuildList<T>(this T[] arr) {
+        Node<T> head = new Node<T>(arr[0]);
+        Node<T> tail = head;
+        for (int i = 1; i < arr.Length; i++) {
+            tail.SetNext(new Node<T>(arr[i]));
+            tail = tail.GetNext();
+        }
+        return head;
+    }
+
+    public static Node<T> Insert<T>(this Node<T> p, T value) {
+        if (p == null) {
+            return new Node<T>(value);
+        }
+        Node<T> next = p.GetNext();
+        Node<T> newNode = new Node<T>(value);
+        p.SetNext(newNode);
+        newNode.SetNext(next);
+        return newNode;
+    }
+
+    public static string ToStringRecursive<T>(this Node<T> head) {
+        string rest = head.GetNext() != null
+            ? head.GetNext().ToStringRecursive()
+            : "\b\b";
+
+        return $"({head}, {rest})";
+    }
+
+    public static void Delete<T>(this Node<T> p, Node<T> target) {
+        if (p.GetNext() == target) {
+            p.SetNext(target.GetNext());
+            return;
+        }
+        Delete(p.GetNext(), target);
+    }
+
+    public static Node<T> Previous<T>(this Node<T> list, Node<T> target) {
+        if (list.GetNext() == null) return null;
+        if (list.GetNext() == target) return list;
+
+        return Previous(list.GetNext(), target);
+    }
+
+    public static int LengthToNull<T>(this Node<T> list) {
+        if (list.GetNext() == null) return 0;
+        return LengthToNull(list.GetNext()) + 1;
+    }
+
+    public static Node<int> Intersection(this Node<int> first, Node<int> second) {
+        Node<int> head = new Node<int>(0);
+        Node<int> tail = head;
+        while (first != null && second != null) {
+            int firstValue = first.GetInfo();
+            int secondValue = second.GetInfo();
+
+            if (firstValue == secondValue) {
+                tail.SetNext(new Node<int>(first.GetInfo()));
+                tail = tail.GetNext();
+
+                first = first.GetNext();
+                second = second.GetNext();
+            } else if (firstValue > secondValue) {
+                second = second.GetNext();
+            } else {
+                first = first.GetNext();
+            }
+        }
+        return head.GetNext();
+    }
+}
--- Irena/Library/Program.cs
+++ Irena/Library/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using Unit4.CollectionsLib;
 
-namespace Library
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr = { 1, 2, 54, 2, 6 };
+namespace Library {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] arr = { 1, 2, 54, 76, 80 };
+            int[] arr2 = { 1, 4, 54, 80 };
             Node<int> node = arr.BuildList();
             Console.WriteLine(node.ToStringRecursive());
             node.Delete(node.GetNext().GetNext());
             Console.WriteLine("delete third element");
             Console.WriteLine(node.ToStringRecursive());
             Console.WriteLine(node.Previous(node.GetNext().GetNext().GetNext()).ToStringRecursive());
+
+            Console.WriteLine(arr.BuildList().Intersection(arr2.BuildList()).ToStringRecursive());
         }
     }
 }
