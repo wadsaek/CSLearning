27c694520bfa0821ba9bfc70921cfb183faa0331
wadsaek <wadsaek@gmail.com>
2025-01-21 10:28:29 +0200

    RangeNode exercise

---
+++ Irena/08.01.2025/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unit4.CollectionsLib;
+
+namespace _08._01._2025;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static Node<RangeNode> RangeList(Node<int> list) {
+        int first = list.GetInfo();
+        list = list.GetNext();
+        Node<RangeNode> head = new Node<RangeNode>(new RangeNode(first, first));
+        Node<RangeNode> tail = head;
+
+        while (list != null) {
+            int current = list.GetInfo();
+            RangeNode currentRange = tail.GetInfo();
+            if (current == currentRange.to + 1) {
+                currentRange.to = current;
+            } else {
+                RangeNode nextRange = new RangeNode(current, current);
+                tail.SetNext(new Node<RangeNode>(nextRange));
+                tail = tail.GetNext();
+            }
+            list = list.GetNext();
+        }
+        return head;
+    }
+
+}
---
+++ Irena/08.01.2025/RangeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace _08._01._2025;
+
+public class RangeNode {
+    public int to {get; set;}
+    public int from {get;set;}
+    public RangeNode(int to, int from){
+        this.to = to;
+        this.from = from;
+    }
+}
