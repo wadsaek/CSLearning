0710a6f8f92ac1a244427a629575d7e4b6d204d7
wadsaek <wadsaek@gmail.com>
2025-03-03 12:04:12 +0200

    cage

---
+++ Irena/20.01.2025/Cage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _20._01._2025;
+
+public class Cage {
+    public Cage(string name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public string name {get; set;}
+    public int id {get; set;}
+
+    public override bool Equals(object obj) {
+        return obj is Cage cage &&
+               name == cage.name &&
+               id == cage.id;
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(name, id);
+    }
+
+    public override string ToString() {
+        return $"{name} with id {id}";
+    }
+
+    private string GetDebuggerDisplay() {
+        return ToString();
+    }
+}
---
+++ Irena/20.01.2025/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Unit4.CollectionsLib;
+using Library;
+
+namespace _20._01._2025;
+
+class Program {
+    static void Main(string[] args) {
+
+    }
+
+    //static void Group<T>(Node<T> node, Func<T, T, bool> areGrouped) {
+    //    Node<T> pointer = node;
+    //    Node<T> groupExamples = new Node<T>(node.GetInfo());
+    //    while (pointer is not null) {
+    //        if (areGrouped(pointer.GetInfo(), pointer.GetNext().GetInfo())) {
+    //            pointer = pointer.GetNext();
+    //            continue;
+    //        }
+    //
+    //        if (groupExamples.Contains((x) => areGrouped(
+    //                        x.GetInfo(),
+    //                        pointer.GetNext().GetInfo()
+    //                    ))) {
+    //
+    //        }
+    //    }
+    //}
+
+    //static bool UniqueGroups(Node<int> node, int k) {
+    //    int len = node.LengthToNull();
+    //    if (len % k != 0) return false;
+
+    //    Node<int>[] arr = new Node<int>[k / 4];
+    //    Node<int>[] tails = new Node<int>[arr.Length];
+    //    for (int counter = 0; node is not null; node = node.GetNext()) {
+    //        if(arr[counter/4] is null){
+
+    //        } 
+    //    }
+    //}
+
+
+}
